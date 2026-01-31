cfe915543b251c14e9653e6c1f2b9d817ed8cdfe
Bogdan Bogush <bogdan.s.bogush@gmail.com>
2026-01-31 17:53:03 +0200

    Add docker file

--- README.md
+++ README.md
@@ -1,8 +1,18 @@
 # cpp-project-template
 
-Basic CMake build instructions.
+C++ project template.
+
+## Setup environment
+
+Docker used to have the same environment for development.
+
+```
+./docker/docker_run.sh
+```
 
 ## Build
+
+CMake is most popular build system for C++ projects.
 
 ```bash
 mkdir -p build
@@ -16,7 +26,3 @@ Run the produced executable (name matches project):
 ```bash
 ./hello
 ```
-
-## Build system
-
-CMake is most popular build system for C++ projects.
---
+++ docker/docker_run.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+IMAGE_NAME="cpp-env"
+DOCKERFILE="docker/Dockerfile"
+
+# Exit on error
+set -e
+
+# Check if Docker is installed
+if ! command -v docker &> /dev/null; then
+    echo "❌ Docker is not installed."
+    exit 1
+fi
+
+# Check if image exists and rebuild
+if ! docker image inspect "$IMAGE_NAME" > /dev/null 2>&1; then
+    echo "🟢 Docker image does not exist. Will build."
+    docker build -t "$IMAGE_NAME" -f "$DOCKERFILE" .
+fi
+
+# Run container
+docker run --rm -it \
+    -v "$PWD:/project" \
+    "$IMAGE_NAME" \
+    bash
+
